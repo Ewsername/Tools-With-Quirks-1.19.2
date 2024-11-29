cfa5f4eb41de135c04cf06b87fd8b9f64a6344d1
Ewsername <159178256+Ewsername@users.noreply.github.com>
2024-11-29 20:55:08 +0100

    Initial commit

---
+++ src/main/java/net/bunmuppet/toolswithquirks/ToolsWithQuirksClient.java
@@ -0,0 +1,10 @@
+package net.bunmuppet.toolswithquirks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToolsWithQuirksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
